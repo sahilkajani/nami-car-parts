cd35e7b49132331870f10257c8d0084281615fb1
Sahil <sahilkajani30@gmail.com>
2021-01-04 06:47:10 +0000

    Added new page and code for searching spare parts using vehicles

---
+++ search-results.js
@@ -0,0 +1,43 @@
+// API Reference: https://www.wix.com/corvid/reference
+// “Hello, World!” Example: https://www.wix.com/corvid/hello-world
+
+import wixData from 'wix-data';
+import {local} from 'wix-storage';
+import wixLocation from 'wix-location';
+
+const sparePartItemsRepeater = "#sparePartItemsRepeater";
+var searchResultItems = [];
+
+$w.onReady(function () {	
+
+	$w(sparePartItemsRepeater).hide();
+
+	var vehicleIDs = local.getItem("vehicleIDs");
+
+	var spareParts =  searchSpareParts(vehicleIDs)					
+					
+
+});
+
+export function searchSpareParts(vehicleId){
+
+	wixData.queryReferenced("Vehicles", vehicleId, "product")
+	.then( (results) => {
+		if(results.items.length > 0) {
+
+		var labelText = "(" + results.items.length + " found)"
+		$w("#resultsCountLabel").text = labelText;
+		$w(sparePartItemsRepeater).show();
+		searchResultItems = results.items;
+		$w(sparePartItemsRepeater).data = results.items
+		} 
+		else {
+		$w("#resultsCountLabel").text = "(0 found)"
+		$w(sparePartItemsRepeater).hide();
+		}
+    })
+    .catch( (err) => {
+     let errorMsg = err;
+	 console.log(errorMsg)
+    });
+}
